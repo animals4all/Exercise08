76b6b4f83e2e95092e191c0d71c1fc9a5505b15c
Caroline <animals4all@gmail.com>
2015-07-10 18:27:04 -0400

    Revisions

--- markovtextgenerator.py
+++ markovtextgenerator.py
@@ -4,29 +4,33 @@ def makeChains(corpus, keyLength):
     """Takes an input text as a string and returns a dictionary of
     markov chains."""
     chainDict = {}
-    fileLines = splitIntoWordLists(corpus)
 
-    for line in fileLines:
-        if line == "":
-            continue
-
+    for line in corpus:
         for word in line:
             wordIndex = line.index(word)
+            # Get the words for the next key based on the current index and the key length
             wordGroup = line[wordIndex:wordIndex+keyLength]
 
             if len(wordGroup) < keyLength:
+                # The word selection isn't completely contained within the list, making it shorter.
+                # Shift the selection back by the difference of the keyLength and the length of the
+                # word and use that as the word group
                 wordGroup = line[wordIndex-(keyLength-len(wordGroup)):wordIndex+keyLength-(keyLength-len(wordGroup))]
-                if wordIndex+keyLength-(keyLength-len(wordGroup)) + 1 > len(line):
+                if wordIndex + keyLength - (keyLength - len(wordGroup)) + 1 > len(line):
+                    # There is no word following the selection: the line ends
                     followingWord = None
                 else:
+                    # Use word immediately following selection as key
                     followingWord = line[wordIndex+keyLength-(keyLength-len(wordGroup))]
             elif wordIndex + keyLength + 1 > len(line):
+                # There is no word following the selection: the line ends
                 followingWord = None
             else:
+                # Use word immediately following selection as key
                 followingWord = line[wordIndex + keyLength]
 
             if tuple(wordGroup) not in chainDict:
-                    chainDict[tuple(wordGroup)] = [followingWord]
+                chainDict[tuple(wordGroup)] = [followingWord]
             else:
                 chainDict[tuple(wordGroup)].append(followingWord)
 
@@ -42,61 +46,77 @@ def makeText(chains, partAmount):
 
     for i in range(partAmount):
         for part in partKey:
+            # Add the current part to the random text
             if lineStart:
+                # Capitalize the first letter in the part if it's the start of a new line
                 part = part.capitalize()
                 lineStart = False
             randomText += part + " "
 
         if lineEnd:
+            # The previous line ended, so a random part to start the new line is needed
             partKey = random.choice(chains.keys())
             lineStart = True
         else:
+            # Get a random value from the dict that follows the key. The more frequent it is in the
+            # list, the more likely it is to be chosen.
             partValue = random.choice(chains[partKey])
 
-        if partValue == None or lineEnd == True:
+        if partValue == None:
+            # The part isn't followed by anything, so a random part to start the new line is needed
             partKey = random.choice(chains.keys())
             lineStart = True
         else:
+            # Find a key that starts with the value to add to the random text next.
             matchingKeys = []
             for key in chains.keys():
                 if key[0] == partValue:
                     matchingKeys.append(key)
             if matchingKeys == []:
+                # No key starts with the value. It needs to be added to the text and then a new line
+                # will start
                 lineEnd = True
                 partKey = [partValue]
             else:
+                # Choose a random key from the keys that begin with the value
                 partKey = random.choice(matchingKeys)
 
     return randomText
 
 def splitIntoWordLists(corpus):
+    """Remove all newline characters, split the text into
+    lists of words, and remove the blank lines."""
     fileLines = corpus.split("\n")
     for line in fileLines:
         lineIndex = fileLines.index(line)
         fileLines[lineIndex] = line.split(" ")
+        if fileLines[lineIndex] in ([], [""]):
+            fileLines.remove(fileLines[lineIndex])
 
     return fileLines
 
 def getPartAmount(corpus, keyLength):
     """Get the amount of parts to put into the random text from the
-    chain dictionary."""
-    fileLines = splitIntoWordLists(corpus)
-    line = random.choice(fileLines)
-    for line in fileLines:
-        assert keyLength <= len(line), "keyLength cannot be greater than the amount of words in the longest line"
+    chain dictionary so that the random text has about the same amount
+    of words as the original text"""
+    sampleLine = random.choice(corpus)
 
-    return int(math.ceil(float(len(line))/float(keyLength)))
+    # Round up with math.ceil
+    return int(math.ceil(float(len(sampleLine))/float(keyLength)))
 
 def main():
-    script, fileName = sys.argv
-
-    inputText = open(fileName).read()
+    #script, fileName = sys.argv
+    fileContents = open("testRhyme2.txt").read()
 
     keyLength = 2
-    partAmount = getPartAmount(inputText, keyLength)
 
+    # Change file into lists to count words
+    inputText = splitIntoWordLists(fileContents)
+    for line in inputText:
+        assert keyLength <= len(line), "keyLength cannot be greater than the amount of words in the shortest line"
+
+    partAmount = getPartAmount(inputText, keyLength)
     chainDict = makeChains(inputText, keyLength)
-    print chainDict
     randomText = makeText(chainDict, partAmount)
     print randomText
 
