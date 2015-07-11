3b1b575399e0c506930db69a84e29d88c7880d1f
animals4all <animals4all@gmail.com>
2015-07-11 18:49:11 -0400

    Output Entire Poem

--- markovTextGenerator.py
+++ markovTextGenerator.py
@@ -36,50 +36,57 @@ def makeChains(corpus, keyLength):
 
     return chainDict
 
-def makeText(chains, partAmount):
+def makeText(chains, partAmount, lineNum, emptyLines):
     """Takes a dictionary of markov chains and returns random text
     based off an original text."""
-    randomText = ""
-    partKey = random.choice(chains.keys())
-    lineStart = True
-    lineEnd = False
+    randomText = []
 
-    for i in range(partAmount):
-        for part in partKey:
-            # Add the current part to the random text
-            if lineStart:
-                # Capitalize the first letter in the part if it's the start of a new line
-                part = part.capitalize()
-                lineStart = False
-            randomText += part + " "
+    for l in range(lineNum):
+        partKey = random.choice(chains.keys())
+        sentenceStart = True
+        sentenceEnd = False
+        line = ""
 
-        if lineEnd:
-            # The previous line ended, so a random part to start the new line is needed
-            partKey = random.choice(chains.keys())
-            lineStart = True
-        else:
-            # Get a random value from the dict that follows the key. The more frequent it is in the
-            # list, the more likely it is to be chosen.
-            partValue = random.choice(chains[partKey])
+        # Only add to the line if it's not empty
+        if l not in emptyLines:
+            for i in range(partAmount):
+                for part in partKey:
+                    # Add the current part to the random text
+                    if sentenceStart:
+                        # Capitalize the first letter in the part if it's the start of a new line
+                        part = part.capitalize()
+                        sentenceStart = False
+                    line += part + " "
 
-        if partValue == None:
-            # The part isn't followed by anything, so a random part to start the new line is needed
-            partKey = random.choice(chains.keys())
-            lineStart = True
-        else:
-            # Find a key that starts with the value to add to the random text next.
-            matchingKeys = []
-            for key in chains.keys():
-                if key[0] == partValue:
-                    matchingKeys.append(key)
-            if matchingKeys == []:
-                # No key starts with the value. It needs to be added to the text and then a new line
-                # will start
-                lineEnd = True
-                partKey = [partValue]
-            else:
-                # Choose a random key from the keys that begin with the value
-                partKey = random.choice(matchingKeys)
+                if sentenceEnd:
+                    # The previous sentence ended, so a random part to start the new line is needed
+                    partKey = random.choice(chains.keys())
+                    sentenceStart = True
+                else:
+                    # Get a random value from the dict that follows the key. The more frequent it is in
+                    # the list, the more likely it is to be chosen.
+                    partValue = random.choice(chains[partKey])
+
+                if partValue == None:
+                    # The part isn't followed by anything, so a random part to start the new sentence is
+                    # needed
+                    partKey = random.choice(chains.keys())
+                    sentenceStart = True
+                else:
+                    # Find a key that starts with the value to add to the random text next.
+                    matchingKeys = []
+                    for key in chains.keys():
+                        if key[0] == partValue:
+                            matchingKeys.append(key)
+                    if matchingKeys == []:
+                        # No key starts with the value. It needs to be added to the text and then a new
+                        # sentence will start
+                        sentenceEnd = True
+                        partKey = [partValue]
+                    else:
+                        # Choose a random key from the keys that begin with the value
+                        partKey = random.choice(matchingKeys)
+        randomText.append(line)
 
     return randomText
 
@@ -87,6 +94,7 @@ def splitIntoWordLists(corpus):
     """Remove all newline characters, split the text into
     lists of words, and remove the blank lines."""
     fileLines = corpus.split("\n")
+    lineNum = len(fileLines)
     emptyLines = []
     for line in fileLines:
         lineIndex = fileLines.index(line)
@@ -94,10 +102,11 @@ def splitIntoWordLists(corpus):
         if fileLines[lineIndex] == [""]:
             emptyLines.append(lineIndex)
 
+    # Loop through empty line indexes in reverse order
     for lineIndex in reversed(emptyLines):
         del fileLines[lineIndex]
 
-    return fileLines
+    return fileLines, lineNum, emptyLines
 
 def getPartAmount(corpus, keyLength):
     """Get the amount of parts to put into the random text from the
@@ -115,14 +124,15 @@ def main():
     keyLength = 2
 
     # Change file into lists to count words
-    inputText = splitIntoWordLists(fileContents)
+    inputText, lineNum, emptyLines = splitIntoWordLists(fileContents)
     for line in inputText:
         assert keyLength <= len(line), "keyLength cannot be greater than the amount of words in the shortest line"
 
     partAmount = getPartAmount(inputText, keyLength)
     chainDict = makeChains(inputText, keyLength)
-    randomText = makeText(chainDict, partAmount)
-    print randomText
+    randomText = makeText(chainDict, partAmount, lineNum, emptyLines)
+    for line in randomText:
+        print line
 
 if __name__ == "__main__":
     main()
